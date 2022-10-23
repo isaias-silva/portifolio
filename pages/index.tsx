bf39cf3bf5fbec6d6f71be7a2a61f1ebc18db485
isaias-silva <zackblackmangold@gmail.com>
2022-10-23 15:09:19 -0300

    metatags

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,30 @@ import Head from 'next/head'
 import Section from '../components/section'
 import experience from '../data/experience.json'
 const Home: NextPage = () => {
-  
-  const experiencia = experience.map((x,i)=>{
-return <div className='exp' key={i}>
-<h3>{x.title}</h3>
-<span>{x.cargo}</span>: <span>{x.periodo}</span>
-<p>
- {x.descricao}
-</p>
-</div>
+
+  const experiencia = experience.map((x, i) => {
+    return <div className='exp' key={i}>
+      <h3>{x.title}</h3>
+      <span>{x.cargo}</span>: <span>{x.periodo}</span>
+      <p>
+        {x.descricao}
+      </p>
+    </div>
 
   })
- 
+
 
   return (<>
-  <Head>
-    <title>
-      bem vindo!
-    </title>
-  </Head>
+    <Head>
+      <meta name='description' content='portifólio de zack santos ou isaias santos, desenvolvedor fullstack' />
+      <meta name='author' content='isaias santos' />
+      <meta name='keywords' content='programador, desenvolvedor, fullstack, typescript, javascript, nodejs, freelancer, isaias, zack santos' />
+      <meta name='robots' content='index,follow' />
+      <title>
+        bem vindo!
+      </title>
+
+    </Head>
     <Section img='a' title='a' type='about'></Section>
     <Section element={experiencia} img='https://i.pinimg.com/originals/c0/e1/5f/c0e15fc5b0a8751878fb7ccf17c4fa89.jpg' title='Experience:' type='normal'></Section>
   </>)
