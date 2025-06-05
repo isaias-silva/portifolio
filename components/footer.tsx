27b241c8a5c51f532b11c9c5149c2d62f8c58454
isaias <isaiasgarraeluta@gmail.com>
2025-06-05 11:55:39 -0300

    fix: :recycle: ajust json

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,8 +9,7 @@ export default function Footer(){
 <input type="hidden" name="no_recurring" value="0" />
 <input type="hidden" name="item_name" value="ajude o desenvolvedor" />
 <input type="hidden" name="currency_code" value="BRL" />
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-<img alt="" src="https://www.paypal.com/en_BR/i/scr/pixel.gif" width="1" height="1" />
+
 </form>
 
 
